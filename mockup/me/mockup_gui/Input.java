b63fbc90288c730b4014ac2954a8b99b45bf50aa
ole0811 <ole0811sch@gmail.com>
2023-10-31 20:18:07 +0100

    Move mockup input to separate file

---
+++ mockup/me/mockup_gui/Input.java
@@ -0,0 +1,15 @@
+package me.mockup_gui;
+
+import me.build.mockup_gui.KeyBios;
+
+public class Input {
+	public static final int[] keycode_seq = new int[]{ 
+		KeyBios.KEY_CTRL_F3_,
+		KeyBios.KEY_CTRL_DOWN_,
+		KeyBios.KEY_CTRL_DOWN_,
+		KeyBios.KEY_CTRL_DOWN_,
+		KeyBios.KEY_CTRL_DOWN_,
+		KeyBios.KEY_CTRL_DOWN_,
+		KeyBios.KEY_CTRL_DEL_,
+	};
+}
--- mockup/me/mockup_gui/Main.java
+++ mockup/me/mockup_gui/Main.java
@@ -8,15 +8,7 @@ import me.build.mockup_gui.*;
 public class Main {
 	private static int SCREEN_WIDTH;
 	private static int SCREEN_HEIGHT;
-	private static final int[] keycode_seq = new int[]{ 
-		KeyBios.KEY_CTRL_F3_,
-		KeyBios.KEY_CTRL_DOWN_,
-		KeyBios.KEY_CTRL_DOWN_,
-		KeyBios.KEY_CTRL_DOWN_,
-		KeyBios.KEY_CTRL_DOWN_,
-		KeyBios.KEY_CTRL_DOWN_,
-		KeyBios.KEY_CTRL_DEL_,
-	};
+	private static final int[] keycode_seq = me.mockup_gui.Input.keycode_seq;	
 	private static int seq_i = 0;
 	public static final Object sem = new Object();
 
